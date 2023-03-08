e28840704b9b445ce08ffb549bed69315c7ef1e0
Коновалова Лера <73699428+lerapenguin11@users.noreply.github.com>
2023-03-08 23:42:47 +0500

    layout of the main screen

--- app/src/main/java/com/example/kinopoisk/MainActivity.kt
+++ app/src/main/java/com/example/kinopoisk/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.kinopoisk
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.kinopoisk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.films -> replaceFragment(MoviesFragment())
+                R.id.heart -> replaceFragment(FavouritesFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else ->{
+                }
+            }
+            true
+        }
+    }
+
+    @SuppressLint("CommitTransaction")
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
